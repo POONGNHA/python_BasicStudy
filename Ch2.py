3080d519d2bc2d435b2d4046d66ac292d934cf05
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-05-30 18:17:01 +0900

    first day Reviewing python study

---
+++ Ch2.py
@@ -0,0 +1,14 @@
+#Enter must use if grammer form
+a = 10
+b = 10
+
+if a is b:
+    print('10입니다')
+if a == 10:
+    print('10입니다')
+
+#divide and return quotient, remainder with tuple form
+print(divmod(5,2))
+
+#unsigned int, unsigned char means positive number
+#int, char contain both positive , negative number
---
+++ Ch3.py
@@ -0,0 +1,25 @@
+x = 10
+# type imform you type of variable
+print(type(x))
+
+x, y = 10, 20
+#change each variable's value (aka Dynamic Typing)
+x, y = y, x
+print(x); print(y)
+
+#delete variable
+del x
+
+# empty variable, upper N use
+x = None
+print(x)
+
+a = 10
+#equal a = a + 10  -= *= /= //=
+a += 20
+print(a)
+
+
+a = input('Enter String  : ')
+b = input('Enter String  : ')
+print(a)
