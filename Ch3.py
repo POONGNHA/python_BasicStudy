2e09fe61efdc890b33f6ade9e98d57ede768138f
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-05-30 20:43:49 +0900

    2022'05'30 first day Reviewing python study Ch1~3 and study algorithm E-book by Ch5

--- Ch3.py
+++ Ch3.py
@@ -19,7 +19,25 @@ a = 10
 a += 20
 print(a)
 
+#split makes received input to Array
+# a = input('Type array input : ').split(',')
+# print(a)
 
-a = input('Enter String  : ')
-b = input('Enter String  : ')
-print(a)
+# map method( input1, input2) make data to input1 data form.
+
+#map equals FloorMethod in java
+c = [1.2, 2.5, 30.7, 4.6]
+c = list(map(int, c))
+print(c)
+
+#sep locate between elementals
+print(1,2,3,sep='하고 ')
+
+print(4,5,6,sep='\n')
+
+#end makes input1 contiue writing
+print(4,end=' ')
+print(5,end=' ')
+print(6,end=' 끝')
+
+# escape : To put ' (single quotation marks) in '     '     just /' is useful
